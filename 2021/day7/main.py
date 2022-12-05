a0452ca0d1bfee88a12f435546a6f303f6ed24d7
piyush-daga <piyushdaga5@gmail.com>
2022-12-05 19:33:34 +0530

    2021 - Day 7

--- 2021/day7/main.py
+++ 2021/day7/main.py
@@ -1,4 +1,5 @@
 import unittest
+from collections import Counter
 from typing import List
 
 
@@ -9,20 +10,55 @@ class TestSuccess(unittest.TestCase):
 
 
 
-def read_file(fname: str) -> List[str]:
+def read_file(fname: str) -> str:
     with open(fname) as fp:
         data = fp.read().strip()
 
-    return data.split("\n")
+    return data
 
+def frequency_counter(data: List[int]):
+    return Counter(data)
 
-def main(fname: str) -> int:
-    ...
+def fuel_req_to_move(data: List[int], pos: int):
+    result = 0
+
+    for d in data:
+        result += abs(d - pos)
+
+    return result
+
+def crab_engineering_fuel(data: List[int], pos: int):
+    result = 0
+
+    for d in data:
+        steps = abs(d - pos)
+        result += (steps * (steps + 1 )) // 2
+
+    return result
+
+def main(fname: str) -> None:
+    data = read_file(fname)
+    data = [int(d) for d in data.split(",")]
+
+    # freq = frequency_counter(data)
+
+    min = 100_000_00000_000000
+    final_pos = 0
+    for pos in range(max(data)):
+        # fuel_req = fuel_req_to_move(data, pos)
+        fuel_req = crab_engineering_fuel(data, pos)
+        # print("Fuel required to move to pos: %d is %d" % (pos, fuel_req))
+        if fuel_req < min:
+            min = fuel_req
+            final_pos = pos
+
+    print("Fuel required to move to min pos is: ", final_pos, min)
 
 
 if __name__ == "__main__":
-    # main("./input/input_small.txt")
+    # main("./2021/day7/input/input_small.txt")
+    main("./2021/day7/input/input.txt")
 
-    unittest.main()
+    # unittest.main()
 
     # main("./input/input.txt")
